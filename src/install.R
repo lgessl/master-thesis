e976911b9506f759c72d423777d11d6526117be8
gel53287 <lukas.gessl@stud.uni-regensburg.de>
2023-10-25 14:45:42 +0200

    only install if not done yet

--- src/install.R
+++ src/install.R
@@ -1,1 +1,3 @@
-install.packages("https://github.com/rehbergT/zeroSum/raw/main/zeroSum_2.0.6.tar.gz", repos = NULL)
+if(!require("zeroSum")){
+    install.packages("https://github.com/rehbergT/zeroSum/raw/main/zeroSum_2.0.6.tar.gz", repos = NULL)
+}
