4da470e736973c8f7a676c7f3d51eb4859e39aea
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-11-06 16:06:28 +0000

    sufficient performance on test data

--- src/cox-lasso-zerosum/schmitz.R
+++ src/cox-lasso-zerosum/schmitz.R
@@ -2,6 +2,9 @@
 
 library(tidyverse)
 library(zeroSum)
+library(ROCR)
+
+source("src/cox-lasso-zerosum/helpers.R")
 
 data_path <- "data/schmitz"
 train_dir <- "train"
@@ -14,28 +17,27 @@ result_dir <- "results/cox-lasso-zerosum/schmitz"
 if(!dir.exists(result_dir))
     dir.create(result_dir, recursive=TRUE)
 
-expr_tr <- read_csv(file.path(data_path, train_dir, expr_fname))
-pheno_df <- read_csv(file.path(data_path, train_dir, pheno_fname))
-all(colnames(expr_tr) == pheno_df$...1)
+# Training
 
-# prepare for zerosum
-expr_tr <- expr_tr %>% 
-    select(!Gene) %>% 
-    as.matrix() %>% 
-    t()
-y <- pheno_df %>%
-    select(
-        progression_free_survival__pfs__time__yrs, 
-        progression_free_survival__pfs__status__0_no_progressoin__1_progression
-        ) %>%
-    as.matrix()
-colnames(y) <- c("pfs_yrs", "pfs_binary")
-
-
-fit <- zeroSum(
-    x = expr_tr,
-    y = y,
+tr <- read_data(data_path, train_dir)
+tr <- prepare_data(tr$expr, tr$pheno)
+View(tr$y)
+fit <- zeroSum::zeroSum(
+    x = tr$x,
+    y = tr$y,
     family = "cox",
     alpha = 1
 )
-?zeroSum
+saveRDS(fit, file.path(result_dir, "schmitz_fit.rds"))
+
+
+# Testing
+
+te <- read_data(data_path, test_dir)
+te <- prepare_data(te$expr, te$pheno, follow_up = TRUE)
+predicted <- zeroSum::predict(fit, te$x)
+predicted <- ROCR::prediction(predicted, te$y[, "pfs_yrs"] <= 2)
+tpr_vs_prev <- ROCR::performance(predicted, measure = "tpr", x.measure = "rpp")
+sum(te$y[, "pfs_yrs"] <= 2)
+cbind(tpr_vs_prev@x.values[[1]], tpr_vs_prev@y.values[[1]])
+?performance
