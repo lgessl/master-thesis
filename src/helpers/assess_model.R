542d21ec9082629e58819baaa604b6349d71ac41
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-11-10 12:26:13 +0000

    outsource tibble creation

--- src/helpers/assess_model.R
+++ src/helpers/assess_model.R
@@ -1,6 +1,31 @@
 import("ggplot2")
 import("dplyr")
 
+prev_prec_tbl <- function(
+    predicted,
+    labels
+){
+    prev_vs_prec <- ROCR::performance(predicted, measure = "prec", x.measure = "rpp")
+    tbl <- tibble::tibble(
+        "prevalence" = prev_vs_prec@x.values[[1]],
+        "precision" = prev_vs_prec@y.values[[1]],
+        "cutoff" = prev_vs_prec@alpha.values[[1]]
+    )
+    tbl <- tbl %>% filter(!is.na(precision))
+    return(tbl)
+}
+
+roc <- function(predicted, labels){
+    roc <- ROCR::performance(predicted, measure = "tpr", x.measure = "fpr")
+    tbl <- tibble::tibble(
+        "false positive rate" = roc@x.values[[1]],
+        "true positive rate" = roc@y.values[[1]]
+    )
+    tbl <- tbl %>% filter(!is.na(`false positive rate`))
+    return(tbl)
+}
+
+
 assess_model <- function(
     predicted,
     labels,
@@ -9,28 +34,30 @@ assess_model <- function(
     prev_prec_csv = TRUE,
     roc_auc_plot = TRUE,
     show_plots = TRUE,
+    with_points = FALSE,
     width = 7,
     height = 4,
     units = "in",
     csv_round_n_digits = 3
 ){
+    return_lst <- list()
+    # UR colors
+    red <- unicol::uni_regensburg_3["glutrot"]
+    gray <- unicol::uni_regensburg_1["neutralgrau"]
     predicted <- ROCR::prediction(predicted, labels) # S4 class 'prediction'
 
-    # prevalence vs true positive rate
+    # Prevalence vs. precision
+    fname <- file.path(result_dir, "prev_vs_prec.pdf")
+    tbl <- prev_prec_tbl(predicted, labels)
     if(prev_prec_plot){
-        fname <- file.path(result_dir, "prev_vs_prec.pdf")
-        prev_vs_prec <- ROCR::performance(predicted, measure = "prec", x.measure = "rpp")
-        tbl <- tibble::tibble(
-            "prevalence" = prev_vs_prec@x.values[[1]],
-            "precision" = prev_vs_prec@y.values[[1]],
-            "cutoff" = prev_vs_prec@alpha.values[[1]]
-        )
-        tbl <- tbl %>% filter(!is.na(precision))
         plt <- ggplot2::ggplot(
             tbl, 
             ggplot2::aes(x = prevalence, y = precision)
             ) +
-            ggplot2::geom_line(col = "darkblue")
+            ggplot2::geom_line(col = red)
+        if(with_points){
+            plt <- plt + ggplot2::geom_point(col = gray)
+        }
         ggplot2::ggsave(fname, plt, width = width, height = height, units = units)
         if(show_plots){
             print(plt)
@@ -50,22 +77,23 @@ assess_model <- function(
             )
         }
     }
+
     # ROC AUC curve
+    fname <- file.path(result_dir, "roc_auc.pdf")
+    tbl <- roc(predicted, labels)
+    roc <- ROCR::performance(predicted, measure = "tpr", x.measure = "fpr")
+    auc <- ROCR::performance(predicted, measure = "auc")
+    auc <- round(auc@y.values[[1]], digits = 4)
     if(roc_auc_plot){
-        fname <- file.path(result_dir, "roc_auc.pdf")
-        roc <- ROCR::performance(predicted, measure = "tpr", x.measure = "fpr")
-        auc <- ROCR::performance(predicted, measure = "auc")
-        auc <- round(auc@y.values[[1]], digits = 4)
-        tbl <- tibble::tibble(
-            "false positive rate" = roc@x.values[[1]],
-            "true positive rate" = roc@y.values[[1]]
-        )
         plt <- ggplot2::ggplot(
             tbl,
             ggplot2::aes(x = `false positive rate`, y = `true positive rate`)
             ) +
-            ggplot2::geom_line(col = "darkred") +
+            ggplot2::geom_line(col = red) +
             ggplot2::ggtitle(stringr::str_c("AUC = ", auc))
+        if(with_points){
+            plt <- plt + ggplot2::geom_point(col = gray)
+        }
         ggplot2::ggsave(fname, plt, width = width, height = height, units = units)
         if(show_plots){
             print(plt)
