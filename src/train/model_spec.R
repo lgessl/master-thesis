0a5b2010a1a410d1f9829ec49190b916f9cedd03
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-12-11 17:03:13 +0000

    canonical model spec

---
+++ src/train/model_spec.R
@@ -0,0 +1,34 @@
+# General list of ModelSpec objects you can source into any script training models on a 
+# certain data set
+
+model_spec_list <- list(
+    cox_lasso_zerosum = ModelSpec(
+        name = "Cox LASSO zeroSum",
+        fitter = zeroSum::zeroSum,
+        optional_fitter_args = list(family = "cox", alpha = 1),
+        response_type = "survival_censored",
+        save_dir = "cox-lasso-zerosum"
+    ),
+    binomial_lasso_zerosum = ModelSpec(
+        name = "Binomial LASSO zeroSum",
+        fitter = zeroSum::zeroSum,
+        optional_fitter_args = list(family = "binomial", alpha = 1),
+        response_type = "binary",
+        save_dir = "binomial-lasso-zerosum"
+    )
+    # ,
+    # cox_lasso = ModelSpec(
+    #     name = "Cox LASSO",
+    #     fitter = zeroSum::zeroSum,
+    #     optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE),
+    #     response_type = "survival_censored",
+    #     save_dir = "cox-lasso"
+    # ),
+    # binomial_lasso = ModelSpec(
+    #     name = "Binomial LASSO",
+    #     fitter = zeroSum::zeroSum,
+    #     optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE),
+    #     response_type = "binary",
+    #     save_dir = "binomial-lasso"
+    # )
+)
