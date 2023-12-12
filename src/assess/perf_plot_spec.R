b7b173ccf56e31a1373c0d8a0c1e3af6a103e611
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-12-12 16:13:44 +0000

    use assess_train_and_test()

--- src/assess/perf_plot_spec.R
+++ src/assess/perf_plot_spec.R
@@ -1,8 +1,10 @@
 # General PerfPlotSpec you can use to assess the performance of any model
 
 std_pps <- PerfPlotSpec(
-    fname = "results/all/prev_vs_precison.pdf",
+    fname = "the_best.pdf",
     x_metric <- "rpp",
     y_metric <- "prec",
-    pfs_leq <- 2.
+    pfs_leq <- 2.,
+    x_lab = "rate of positive predictions",
+    y_lab = "precision"
 )
--- src/assess/schmitz.R
+++ src/assess/schmitz.R
@@ -6,19 +6,14 @@ source("src/train/model_spec.R") # model_spec_list
 source("src/assess/perf_plot_spec.R") # std_pps
 
 model_spec_list <- prepend_to_save_dir(model_spec_list, "models/schmitz")
-data_spec <- readRDS("data/schmitz/test/data_spec.rds")
+std_pps$fname <- file.path("models/schmitz", std_pps$fname)
 
-compare_models(
+ds_train <- readRDS("data/schmitz/train/data_spec.rds")
+ds_test <- readRDS("data/schmitz/test/data_spec.rds")
+
+assess_train_and_test(
     model_spec_list = model_spec_list,
-    data_spec_list = list(data_spec),
-    perf_plot_spec = std_pps
-)
-
-data_spec <- readRDS("data/schmitz/train/data_spec.rds")
-
-compare_models(
-    model_spec_list = model_spec_list,
-    data_spec_list = list(data_spec),
-    perf_plot_spec = std_pps,
-    model_tree_mirror = c("models", "models")
+    data_spec_train = ds_train,
+    data_spec_test = ds_test,
+    perf_plot_spec_train = std_pps
 )
