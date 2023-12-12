6080c9e307105c7b67bbf0eb7724413cd73f4461
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2023-12-12 16:14:12 +0000

    authorize new models

--- src/train/model_spec.R
+++ src/train/model_spec.R
@@ -15,20 +15,19 @@ model_spec_list <- list(
         optional_fitter_args = list(family = "binomial", alpha = 1),
         response_type = "binary",
         save_dir = "binomial-lasso-zerosum"
+    ),
+    cox_lasso = ModelSpec(
+        name = "Cox LASSO",
+        fitter = zeroSum::zeroSum,
+        optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE),
+        response_type = "survival_censored",
+        save_dir = "cox-lasso"
+    ),
+    binomial_lasso = ModelSpec(
+        name = "Binomial LASSO",
+        fitter = zeroSum::zeroSum,
+        optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE),
+        response_type = "binary",
+        save_dir = "binomial-lasso"
     )
-    # ,
-    # cox_lasso = ModelSpec(
-    #     name = "Cox LASSO",
-    #     fitter = zeroSum::zeroSum,
-    #     optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE),
-    #     response_type = "survival_censored",
-    #     save_dir = "cox-lasso"
-    # ),
-    # binomial_lasso = ModelSpec(
-    #     name = "Binomial LASSO",
-    #     fitter = zeroSum::zeroSum,
-    #     optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE),
-    #     response_type = "binary",
-    #     save_dir = "binomial-lasso"
-    # )
 )
