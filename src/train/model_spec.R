621db44dde1fe9ba10d08ae72ac98413fa74e384
Lukas Gessl <lukas.gessl@stud.uni-regensburg.de>
2024-01-08 15:21:04 +0000

    restructure model file tree

--- src/train/model_spec.R
+++ src/train/model_spec.R
@@ -8,15 +8,32 @@ model_spec_list <- list(
         fitter = zeroSum::zeroSum,
         optional_fitter_args = list(family = "cox", alpha = 1),
         response_type = "survival_censored",
-        save_dir = "cox-lasso-zerosum"
+        save_dir = "cox/1-zerosum/2-0"
+    ),
+    cox_lasso = ModelSpec(
+        name = "Cox LASSO",
+        fitter = zeroSum::zeroSum,
+        optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE),
+        response_type = "survival_censored",
+        save_dir = "cox/0-vanilla/2-0"
     ),
     binomial_lasso_zerosum = ModelSpec(
         name = "Binomial LASSO zeroSum",
         fitter = zeroSum::zeroSum,
         optional_fitter_args = list(family = "binomial", alpha = 1),
         response_type = "binary",
-        save_dir = "binomial-lasso-zerosum"
+        save_dir = "binomial/1-zerosum/2-0"
     ),
+    binomial_lasso = ModelSpec(
+        name = "Binomial LASSO",
+        fitter = zeroSum::zeroSum,
+        optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE),
+        response_type = "binary",
+        save_dir = "binomial/0-vanilla/2-0"
+    )
+)
+
+retired_models <- list(
     clz_disc_ipi_feat = ModelSpec(
         name = "CLZ with disc IPI feat",
         fitter = zeroSumWithPheno,
@@ -24,7 +41,7 @@ model_spec_list <- list(
         include_from_discrete_pheno = c("age>60", "ldh_ratio>1", "ecog_performance_status>1", 
             "n_extranodal_sites>1", "ann_arbor_stage>2"),
         response_type = "survival_censored",
-        save_dir = "clz-disc-ipi-feat"
+        save_dir = "cox/early-int/clz-disc-ipi-feat"
     ),
     blz_disc_ipi_feat = ModelSpec(
         name = "BLZ with disc IPI feat",
@@ -33,16 +50,15 @@ model_spec_list <- list(
         include_from_discrete_pheno = c("age>60", "ldh_ratio>1", "ecog_performance_status>1", 
             "n_extranodal_sites>1", "ann_arbor_stage>2"),
         response_type = "binary",
-        save_dir = "blz-disc-ipi-feat"
+        save_dir = "binomial/3-early-int/blz-disc-ipi-feat"
     ),
-    # retired models
     clz_ipi = ModelSpec(
         name = "CLZ with IPI",
         fitter = zeroSumWithPheno,
         optional_fitter_args = list(family = "cox", alpha = 1),
         include_from_discrete_pheno = c("ipi"),
         response_type = "survival_censored",
-        save_dir = "clz-ipi"
+        save_dir = "cox/3-early-int/clz-ipi"
     ),
     blz_ipi = ModelSpec(
         name = "BLZ with IPI",
@@ -50,7 +66,7 @@ model_spec_list <- list(
         optional_fitter_args = list(family = "binomial", alpha = 1),
         include_from_discrete_pheno = c("ipi"),
         response_type = "binary",
-        save_dir = "blz-ipi"
+        save_dir = "binomial/3-early-int/blz-ipi"
     ),
     clz_cont_ipi_features = ModelSpec(
         name = "CLZ with cont IPI feat",
@@ -59,7 +75,7 @@ model_spec_list <- list(
         include_from_continuous_pheno = c("age", "ldh_ratio", "ecog_performance_status", 
             "n_extranodal_sites", "ann_arbor_stage"),
         response_type = "survival_censored",
-        save_dir = "clz-cont-ipi-features"
+        save_dir = "cox/3-early-int/clz-cont-ipi-features"
     ),
     blz_cont_ipi_features = ModelSpec(
         name = "BLZ with cont IPI feat",
@@ -68,7 +84,7 @@ model_spec_list <- list(
         include_from_continuous_pheno = c("age", "ldh_ratio", "ecog_performance_status", 
             "n_extranodal_sites", "ann_arbor_stage"),
         response_type = "binary",
-        save_dir = "blz-cont-ipi-features"
+        save_dir = "binomial/3-early-int/blz-cont-ipi-features"
     ),
     clz_ipi_group = ModelSpec(
         name = "CLZ with IPI group",
@@ -76,7 +92,7 @@ model_spec_list <- list(
         optional_fitter_args = list(family = "cox", alpha = 1),
         include_from_discrete_pheno = c("ipi_group"),
         response_type = "survival_censored",
-        save_dir = "clz-ipi-group"
+        save_dir = "cox/3-early-int/clz-ipi-group"
     ),
     blz_ipi_group = ModelSpec(
         name = "BLZ with IPI group",
@@ -84,20 +100,6 @@ model_spec_list <- list(
         optional_fitter_args = list(family = "binomial", alpha = 1),
         include_from_discrete_pheno = c("ipi_group"),
         response_type = "binary",
-        save_dir = "blz-ipi-group"
-    ),
-    cox_lasso = ModelSpec(
-        name = "Cox LASSO",
-        fitter = zeroSum::zeroSum,
-        optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE),
-        response_type = "survival_censored",
-        save_dir = "cox-lasso"
-    ),
-    binomial_lasso = ModelSpec(
-        name = "Binomial LASSO",
-        fitter = zeroSum::zeroSum,
-        optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE),
-        response_type = "binary",
-        save_dir = "binomial-lasso"
+        save_dir = "binomial/3-early-int/blz-ipi-group"
     )
 )
