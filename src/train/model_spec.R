3347ef4c847360b7c8ccfb64fd3c022d8b4a066a
Lukas Gessl <lukas.gessl@icloud.com>
2024-02-19 12:25:09 +0000

    systemize models: no glmnet (zerosum  is proven to equivalent), same cutoffs for all basic models, include Inf as cutoff for Cox models

--- src/train/model_spec.R
+++ src/train/model_spec.R
@@ -1,20 +1,22 @@
 # General list of ModelSpec objects you can source into any script training models on a 
 # certain data set
 
-# HOPEFULS
+n_genes <- 25066
+
+# BASIC models (only genomic data)
 
 # COX
 # vanilla
-cox_vanilla_zerosum = ModelSpec(
+cox = ModelSpec(
     name = "cox vanilla zerosum",
     directory = "cox/0-vanilla/zerosum",
     fitter = zeroSum::zeroSum,
     split_index = 1:15, # 1:20
-    time_cutoffs = c(seq(1, 2, .25), Inf), # seq(1.5, 2, .25)
+    time_cutoffs = c(seq(1, 2.5, .25), Inf), # seq(1.5, 2, .25)
     optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE),
     response_type = "survival_censored"
 )
-cox_vanilla_std = ModelSpec(
+cox_std = ModelSpec(
     name = "cox vanilla std",
     directory = "cox/0-vanilla/std",
     fitter = zeroSum::zeroSum,
@@ -22,15 +24,6 @@ cox_vanilla_std = ModelSpec(
     time_cutoffs = c(seq(1, 2, .25), Inf),
     optional_fitter_args = list(family = "cox", alpha = 1, zeroSum = FALSE,
         standardize = TRUE),
-    response_type = "survival_censored"
-)
-cox_vanilla_glmnet = ModelSpec(
-    name = "cox vanilla glmnet",
-    directory = "cox/0-vanilla/glmnet",
-    fitter = glmnet::cv.glmnet,
-    split_index = 1:15,
-    time_cutoffs = c(seq(1.25, 2, .25), Inf),
-    optional_fitter_args = list(family = "cox", alpha = 1),
     response_type = "survival_censored"
 )
 # zerosum
@@ -45,7 +38,7 @@ cox_zerosum = ModelSpec(
 )
 # LOGISTIC
 # vanilla
-logistic_vanilla_zerosum = ModelSpec(
+logistic = ModelSpec(
     name = "logistic vanilla zerosum",
     directory = "logistic/0-vanilla/zerosum",
     fitter = zeroSum::zeroSum,
@@ -54,7 +47,7 @@ logistic_vanilla_zerosum = ModelSpec(
     optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE),
     response_type = "binary"
 )
-logistic_vanilla_std = ModelSpec(
+logistic_std = ModelSpec(
     name = "logistic vanilla std",
     directory = "logistic/0-vanilla/std",
     fitter = zeroSum::zeroSum,
@@ -62,15 +55,6 @@ logistic_vanilla_std = ModelSpec(
     time_cutoffs = seq(1, 2, .25),
     optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE,
         standardize = TRUE),
-    response_type = "binary"
-)
-logistic_vanilla_glmnet = ModelSpec(
-    name = "logistic vanilla glmnet",
-    directory = "logistic/0-vanilla/glmnet",
-    fitter = zeroSum::zeroSum,
-    split_index = 1:15,
-    time_cutoffs = seq(1.25, 2, .25),
-    optional_fitter_args = list(family = "binomial", alpha = 1),
     response_type = "binary"
 )
 # zerosum
@@ -84,132 +68,97 @@ logistic_zerosum = ModelSpec(
     response_type = "binary"
 )
 
-lv_disc_ipi_feat = ModelSpec(
-    name = "LV with disc IPI feat",
-    fitter = zeroSum::zeroSum,
-    directory = "logistic/3-early-int/vanilla-disc-ipi-feat",
-    split_index = 1:15,
-    time_cutoffs = c(1.5, 1.75),
-    optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE, 
-        standardize = TRUE),
-    include_from_discrete_pheno = c("age>60", "ldh_ratio>1", "ecog_performance_status>1", 
-        "n_extranodal_sites>1", "ann_arbor_stage>2"),
-    response_type = "binary"
-)
-
-model_spec_list <- list(
+basic_msl <- list(
     # COX
     # vanilla
-    cox_vanilla_zerosum = cox_vanilla_zerosum,
-    cox_vanilla_std = cox_vanilla_std,
-    cox_vanilla_glmnet = cox_vanilla_glmnet,
+    cox = cox,
+    cox_std = cox_std,
     # zerosum
     cox_zerosum = cox_zerosum,
     # LOGISTIC
     # vanilla
-    logistic_vanilla_zerosum = logistic_vanilla_zerosum,
-    logistic_vanilla_std = logistic_vanilla_std,
-    logistic_vanilla_glmnet= logistic_vanilla_glmnet,
+    logistic = logistic,
+    logistic_std = logistic_std,
     # zerosum
-    logistic_zerosum = logistic_zerosum,
-    # early IPI integration
-    lv_disc_ipi_feat = lv_disc_ipi_feat
+    logistic_zerosum = logistic_zerosum
 )
 
 
-# RETIRED
+# EARLY INTEGRATION
 
-# early IPI integration
-# discretized IPI features (as in paper)
-clz_disc_ipi_feat = ModelSpec(
-    name = "CLZ with disc IPI feat",
-    directory = "cox/early-int/clz-disc-ipi-feat",
-    fitter = zeroSumWithPheno,
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "cox", alpha = 1),
+# ipi
+
+# add discretized ipi features (as in paper)
+# logistic
+# ipi penalty = 0
+lv_disc_ipi_feat = ModelSpec(
+    name = "LV with disc IPI feat",
+    fitter = zeroSum::zeroSum,
+    directory = "logistic/3-early-int/vanilla-disc-ipi-feat",
+    split_index = 1:5,
+    time_cutoffs = c(1.5, 1.75),
+    optional_fitter_args = list(family = "binomial", alpha = 1, zeroSum = FALSE, 
+        penalty.factor = c(rep(1, n_genes), rep(0, 5))),
     include_from_discrete_pheno = c("age>60", "ldh_ratio>1", "ecog_performance_status>1", 
         "n_extranodal_sites>1", "ann_arbor_stage>2"),
-    response_type = "survival_censored"
-)
-# just IPI as one feature
-clz_ipi = ModelSpec(
-    name = "CLZ with IPI",
-    fitter = zeroSumWithPheno,
-    directory = "cox/3-early-int/clz-ipi",
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "cox", alpha = 1),
-    include_from_discrete_pheno = c("ipi"),
-    response_type = "survival_censored"
-)
-llz_ipi = ModelSpec(
-    name = "LLZ with IPI",
-    fitter = zeroSumWithPheno,
-    directory = "logistic/3-early-int/llz-ipi",
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "binomial", alpha = 1),
-    include_from_discrete_pheno = c("ipi"),
     response_type = "binary"
 )
-# continuous IPI features
-clz_cont_ipi_features = ModelSpec(
-    name = "CLZ with cont IPI feat",
-    fitter = zeroSumWithPheno,
-    directory = "cox/3-early-int/clz-cont-ipi-features",
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "cox", alpha = 1),
-    include_from_continuous_pheno = c("age", "ldh_ratio", "ecog_performance_status", 
-        "n_extranodal_sites", "ann_arbor_stage"),
-    response_type = "survival_censored"
-)
-llz_cont_ipi_features = ModelSpec(
-    name = "LLZ with cont IPI feat",
-    directory = "logistic/3-early-int/llz-cont-ipi-features",
-    fitter = zeroSumWithPheno,
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "binomial", alpha = 1),
-    include_from_continuous_pheno = c("age", "ldh_ratio", "ecog_performance_status", 
-        "n_extranodal_sites", "ann_arbor_stage"),
-    response_type = "binary"
-)
-# just IPI group as one feature
-clz_ipi_group = ModelSpec(
-    name = "CLZ with IPI group",
-    directory = "cox/3-early-int/clz-ipi-group",
-    fitter = zeroSumWithPheno,
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "cox", alpha = 1),
-    include_from_discrete_pheno = c("ipi_group"),
-    response_type = "survival_censored"
-)
-llz_ipi_group = ModelSpec(
-    name = "LLZ with IPI group",
-    directory = "logistic/3-early-int/llz-ipi-group",
-    fitter = zeroSumWithPheno,
-    split_index = 1,
-    time_cutoffs = 2.,
-    optional_fitter_args = list(family = "binomial", alpha = 1),
-    include_from_discrete_pheno = c("ipi_group"),
-    response_type = "binary"
-)
+# standardize
+lv_disc_ipi_feat_std <- lv_disc_ipi_feat
+lv_disc_ipi_feat_std$name <- "LV with disc IPI feat std"
+lv_disc_ipi_feat_std$directory <- "logistic/3-early-int/vanilla-disc-ipi-feat-std"
+lv_disc_ipi_feat_std$optional_fitter_args$standardize <- TRUE
+lv_disc_ipi_feat_std$optional_fitter_args$penalty.factor <- NULL
+# same for cox
+# ipi penalty = 0
+cv_disc_ipi_feat <- lv_disc_ipi_feat
+cv_disc_ipi_feat$name <- "CV with disc IPI feat"
+cv_disc_ipi_feat$directory <- "cox/3-early-int/vanilla-disc-ipi-feat"
+cv_disc_ipi_feat$optional_fitter_args$family <- "cox"
+cv_disc_ipi_feat$response_type <- "survival_censored"
+# standardize
+cv_disc_ipi_feat_std <- lv_disc_ipi_feat_std
+cv_disc_ipi_feat_std$name <- "CV with disc IPI feat std"
+cv_disc_ipi_feat_std$directory <- "cox/3-early-int/vanilla-disc-ipi-feat-std"
+cv_disc_ipi_feat_std$optional_fitter_args$family <- "cox"
+cv_disc_ipi_feat_std$response_type <- "survival_censored"
 
-retired_models <- list(
-    # early IPI integration
-    # discretized IPI features (as in paper)
-    clz_disc_ipi_feat,
-    lv_disc_ipi_feat,
-    # just IPI as one feature
-    clz_ipi,
-    llz_ipi,
-    # continuous IPI features
-    clz_cont_ipi_features,
-    llz_cont_ipi_features,
-    # just IPI group as one feature
-    clz_ipi_group,
-    llz_ipi_group
+# add ipi as one continuous feature
+# logistic
+# ipi penalty = 0
+lv_ipi_cont <- lv_disc_ipi_feat
+lv_ipi_cont$name <- "LV with IPI cont"
+lv_ipi_cont$directory <- "logistic/3-early-int/vanilla-ipi-cont"
+lv_ipi_cont$optional_fitter_args$penalty.factor <- c(rep(1, n_genes), 0)
+lv_ipi_cont$include_from_continuous_pheno <- "ipi"
+lv_ipi_cont$include_from_discrete_pheno <- NULL
+# standardize
+lv_ipi_cont_std <- lv_ipi_cont
+lv_ipi_cont_std$name <- "LV with IPI cont std"
+lv_ipi_cont_std$directory <- "logistic/3-early-int/vanilla-ipi-cont-std"
+lv_ipi_cont_std$optional_fitter_args$standardize <- TRUE
+lv_ipi_cont_std$optional_fitter_args$penalty.factor <- NULL
+# same for cox
+# ipi penalty = 0
+cv_ipi_cont <- lv_ipi_cont
+cv_ipi_cont$name <- "CV with IPI cont"
+cv_ipi_cont$directory <- "cox/3-early-int/vanilla-ipi-cont"
+cv_ipi_cont$optional_fitter_args$family <- "cox"
+cv_ipi_cont$response_type <- "survival_censored"
+# standardize
+cv_ipi_cont_std <- lv_ipi_cont_std
+cv_ipi_cont_std$name <- "CV with IPI cont std"
+cv_ipi_cont_std$directory <- "cox/3-early-int/vanilla-ipi-cont-std"
+cv_ipi_cont_std$optional_fitter_args$family <- "cox"
+cv_ipi_cont_std$response_type <- "survival_censored"
+
+ei_msl <- list(
+    lv_disc_ipi_feat = lv_disc_ipi_feat,
+    lv_disc_ipi_feat_std = lv_disc_ipi_feat_std,
+    cv_disc_ipi_feat = cv_disc_ipi_feat,
+    cv_disc_ipi_feat_std = cv_disc_ipi_feat_std,
+    lv_ipi_cont = lv_ipi_cont,
+    lv_ipi_cont_std = lv_ipi_cont_std,
+    cv_ipi_cont = cv_ipi_cont,
+    cv_ipi_cont_std = cv_ipi_cont_std
 )
