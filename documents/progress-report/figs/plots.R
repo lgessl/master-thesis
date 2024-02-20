14259442e0992193ff13e5393fab612935f0cd65
gel53287 <lukas.gessl@stud.uni-regensburg.de>
2024-02-20 17:55:57 +0100

    first draft of progress report done, added Makefile incl latexmk

--- documents/progress-report/figs/plots.R
+++ documents/progress-report/figs/plots.R
@@ -2,17 +2,18 @@
 
 library(lymphomaSurvivalPipeline)
 
-data_spec <- readRDS("data/schmitz/data_spec.rds")
-source("src/train/model_spec.R") # logistic@1.75
-source("src/assess/ass_spec.R") # prec_ci_as2, rpp_prec_as2, logrank_as2
+data_spec <- readRDS("../../data/schmitz/data_spec.rds")
+source("../../src/train/model_spec.R") # logistic@1.75
+source("../../src/assess/ass_spec.R") # prec_ci_as2, rpp_prec_as2, logrank_as2
 
-dir <- "documents/progress-report/figs"
+dir <- "figs"
 
-logistic$directory <- file.path("models/schmitz", logistic$directory, "1-75")
+logistic$directory <- file.path("../../models/schmitz", logistic$directory, "1-75")
 logistic$time_cutoffs <- 1.75
 logistic$name <- c("logistic, T = 1.75")
 
 data_spec$cohort <- "test"
+data_spec$directory <- file.path("../..", data_spec$directory)
 data <- read(data_spec)
 
 for(as2 in as2_list){
@@ -28,3 +29,4 @@ for(as2 in as2_list){
         ass_spec_2d = as2
     )
 }
+#
