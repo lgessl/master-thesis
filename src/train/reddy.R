28b18e3503dfcd5433c6e6a5749fee9c68cf5265
Lukas Gessl <lukas.gessl@icloud.com>
2024-05-27 15:13:45 +0000

    Reddy: train first part of li models

--- src/train/reddy.R
+++ src/train/reddy.R
@@ -6,11 +6,11 @@ library(patroklos)
 data <- readRDS("data/reddy/data.rds")
 data$cohort <- "train"
 
-model_groups <- "basic" # c("li", "basic", "ei")
+model_groups <- c("basic", "ei")
 all_models <- list()
 for (mg in model_groups) {
     source(paste0("src/models/", mg, ".R"))
-    all_models <- c(all_models, the_best)
+    all_models <- c(all_models, for_reddy)
 }
 prepend_to_directory(all_models, "models/reddy") 
 training_camp(all_models, data, skip_on_error = TRUE)
